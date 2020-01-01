04ac71fdc23b53de6a1a96ebb05b5cd5c7160295
Khaled Hosny <khaledhosny@eglug.org>
2020-01-02 01:18:24 +0200

    Correctly define Module object

--- docs/index.js
+++ docs/index.js
@@ -18,44 +18,44 @@
 
 import * as TextView from "./TextView.js"
 
-let fontFile = "RanaKufi";
+window.Module = {
+"onRuntimeInitialized": function() {
+    let view = new TextView.View();
+    view.update();
 
-Module["onRuntimeInitialized"] = function() {
-  let view = new TextView.View();
-  view.update();
+    view.canvas.addEventListener('keydown', function(e) {
+      if (e.key === "ArrowLeft")
+        view.moveCursor(1);
+      else if (e.key === "ArrowRight")
+        view.moveCursor(-1);
+      else if (e.key === "Backspace") {
+        e.preventDefault();
+        view.backspace();
+      }
+    });
 
-  view.canvas.addEventListener('keydown', function(e) {
-    if (e.key === "ArrowLeft")
-      view.moveCursor(1);
-    else if (e.key === "ArrowRight")
-      view.moveCursor(-1);
-    else if (e.key === "Backspace") {
-      e.preventDefault();
-      view.backspace();
-    }
-  });
+    view.canvas.addEventListener('click', e => view.setCursorAtPoint(e.clientX));
+    view.canvas.addEventListener('keypress', e => view.insert(e.key));
 
-  view.canvas.addEventListener('click', e => view.setCursorAtPoint(e.clientX));
-  view.canvas.addEventListener('keypress', e => view.insert(e.key));
+    document.getElementById("open").addEventListener("click", e => view.open(e.value));
+    document.getElementById("save").addEventListener("click", e => view.save());
+    document.getElementById("export").addEventListener("click", e => view.export());
 
-  document.getElementById("open").addEventListener("click", e => view.open(e.value));
-  document.getElementById("save").addEventListener("click", e => view.save());
-  document.getElementById("export").addEventListener("click", e => view.export());
+    document.addEventListener('paste', e => {
+      let target = e.explicitOriginalTarget;
+      if (target.id === "canvas") {
+        let text = (e.clipboardData || window.clipboardData).getData('text');
+        view.insert(text);
+      }
+    });
 
-  document.addEventListener('paste', e => {
-    let target = e.explicitOriginalTarget;
-    if (target.id === "canvas") {
-      let text = (e.clipboardData || window.clipboardData).getData('text');
-      view.insert(text);
-    }
-  });
+    [].forEach.call(document.getElementsByClassName("opts"), function(el) {
+      el.addEventListener("change", e => view.update());
+    });
 
-  [].forEach.call(document.getElementsByClassName("opts"), function(el) {
-    el.addEventListener("change", e => view.update());
-  });
-
-  let range = document.getElementById("font-size-range");
-  let number = document.getElementById("font-size");
-  range.addEventListener('input', e => number.value = e.target.value);
-  number.addEventListener('input', e => range.value = e.target.value);
+    let range = document.getElementById("font-size-range");
+    let number = document.getElementById("font-size");
+    range.addEventListener('input', e => number.value = e.target.value);
+    number.addEventListener('input', e => range.value = e.target.value);
+  }
 };
